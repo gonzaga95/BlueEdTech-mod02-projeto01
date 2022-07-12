0e6e914c6851914e9f582f6e9637dad1ac16d950
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-12 17:44:42 -0400

    nav bar funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ const iconGithub = document.querySelector(".bi-github");
 const iconLinkedin = document.querySelector(".bi-linkedin");
 const iconEmail = document.querySelector(".bi-envelope");
 
+iconGithub.addEventListener("mouseover", function () {
+    iconGithub.document.style = "transform: "
+})
